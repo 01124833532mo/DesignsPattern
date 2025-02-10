f25aa088aadd5226f1230b5b37af093aaf68b918
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-02-10 06:24:43 +0200

    Simple Implementation For Singlton Design Pattern

--- SingletonDesignPattern/Program.cs
+++ SingletonDesignPattern/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Static Methode
+
+            GCard gCard1 = GCard.GetCard();
+            GCard gCard2 = GCard.GetCard();
+            GCard gCard3 = GCard.GetCard();
+
+            Console.WriteLine(gCard1.GetHashCode());
+
+            Console.WriteLine(gCard2.GetHashCode());
+            Console.WriteLine(gCard3.GetHashCode());
+            #endregion
+
+            GCard gCard4 = GCard.SingeltonObj;
+            GCard gCard5 = GCard.SingeltonObj;
+            GCard gCard6 = GCard.SingeltonObj;
+
+            Console.WriteLine(gCard4.GetHashCode());
+
+            Console.WriteLine(gCard5.GetHashCode());
+            Console.WriteLine(gCard6.GetHashCode());
+
+
         }
     }
 }
