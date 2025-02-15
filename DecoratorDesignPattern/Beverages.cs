5c88bad2ad5c104e2e6cb96fdb2a9cdb79af185d
mohamed_hamdi_mahmoud <mohammedhamdi726@gmail.com>
2025-02-15 02:05:45 +0200

    Implement Decorator Design Pattern

---
+++ DecoratorDesignPattern/Beverages.cs
@@ -0,0 +1,47 @@
+﻿namespace DecoratorDesignPattern
+{
+    // Abstract Component
+    public abstract class Beverage
+    {
+        public virtual string Description { get; set; } = null!;
+        public virtual decimal Cost { get; set; }
+
+        public override string ToString() => $"{Description} \nCost: {Cost:c}";
+
+
+    }
+    class Espresso : Beverage
+    {
+        public Espresso()
+        {
+            Description = "Espresso Coffee ";
+            Cost = 2.5M;
+        }
+    }
+    class MediumRoastCoffee : Beverage
+    {
+        public MediumRoastCoffee()
+        {
+            Description = "Medium Roast Coffee";
+            Cost = 4.5M;
+        }
+    }
+
+    class Decafe : Beverage
+    {
+        public Decafe()
+        {
+            Description = "Decafe Coffee";
+            Cost = 4.0M;
+        }
+    }
+
+    class DarkRoastCoffee : Beverage
+    {
+        public DarkRoastCoffee()
+        {
+            Description = "Dark Roast Coffee";
+            Cost = 2.5M;
+        }
+    }
+}
